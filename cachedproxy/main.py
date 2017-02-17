f9bf90164557d4176a99327f2c1468137b5b478a
Shiplu Mokaddim <shiplu@mokadd.im>
2017-02-17 03:24:04 +0100

    Load configuration from tornado options
    Use standard way of loading options in tornado app
    we are loading configuration server and app environtment

--- cachedproxy/main.py
+++ cachedproxy/main.py
@@ -1,16 +1,30 @@
 import tornado.ioloop
 import tornado.web
+from tornado.options import define, parse_command_line, options
+import pprint
+from cachedproxy import config
+from cachedproxy import environment
+
+
+define("env", default="local",
+       help="Application environment. One of these values %s"
+       % list(environment.ENVIRONTMENTS.keys()))
+
+define("config-server", default=None,
+       help="Configuration server (Consul) hostname. If no server is provided or its unreachable use 'config.ini' file")
 
 
 class EventWithSubscription(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world")
+        self.write("Settings:\n")
+        self.write(pprint.pformat(self.settings, indent=4))
 
 
 def main():
+    parse_command_line()
     app = tornado.web.Application([
         (r"/events-with-subscriptions/([^/]+)", EventWithSubscription),
-    ])
+    ], settings={"config": config.factory(options.env)})
     app.listen(8888)
     tornado.ioloop.IOLoop.current().start()
 
